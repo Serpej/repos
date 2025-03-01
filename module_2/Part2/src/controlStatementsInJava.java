fe466d9fbbddb92ffa0d10343cf7290c1b354200
Serpej <jehag@live.se>
2025-03-01 11:16:16 +0100

    Add switch statement for operator
    Although there is a problem with the tokenization
    in the scanner.

--- module_2/Part2/src/controlStatementsInJava.java
+++ module_2/Part2/src/controlStatementsInJava.java
@@ -10,20 +10,21 @@ public class controlStatementsInJava {
         double operand1 = 0.0;
         double operand2 = 0.0;
         String falseInput = "";
+        char operator;
         
-        System.out.println("Enter a number for your first operand: ");
+        System.out.println("Enter a number for your first operand  (0-100): ");
 
         if (!userInput.hasNextDouble()) {
             while (!userInput.hasNextDouble()) {
                 falseInput = userInput.next(); // You have to store the input to negate an infinite loop
-                System.out.println("You need to enter a number, enter a first operand: ");
+                System.out.println("You need to enter a number, enter a first operand (0-100): ");
             }
       
         } else {
             operand1 = userInput.nextDouble();
         }
 
-        System.out.printf("Cool, you chose %.0f. Now enter a number for your second operand: %n", operand1);
+        System.out.printf("Cool, you chose %.0f. Now enter a number for your second operand (0-100): %n", operand1);
 
         if (!userInput.hasNextDouble()) {
             while (!userInput.hasNextDouble()) {
@@ -34,6 +35,46 @@ public class controlStatementsInJava {
         } else {
             operand2 = userInput.nextDouble();
         }
-    
+        
+        if (operand1 >= 0 && operand1 <= 100 && operand2 >= 0 && operand2 <= 100) {
+            System.out.println("Alright, and now your operator (+, -, *, /, %): ");
+            //System.out.printf("Alright, %.0f and %.0f, and now your operator (+, -, *, /, %): %n", operand1, operand2);
+
+            operator = userInput.next().charAt(0);
+
+            switch (operator) {
+                case '+':
+                System.out.println("The result is " + (operand1 + operand2));
+                    break;
+                
+                case '-':
+                System.out.println("The result is " + (operand1 - operand2));
+                    break;
+
+                case '*':
+                System.out.println("The result is " + (operand1 * operand2));
+                    break;
+                
+                case '/':
+                System.out.println("The result is " + (operand1 / operand2));
+                    break;
+
+                case '%':
+                System.out.println("The result is " + (operand1 % operand2));
+                    break;
+        
+                default:
+                System.out.println("Invalid input, try again: ");
+                System.out.println(operand1 + " " + operand2 + " " + operator);
+
+
+                    break;
+            }
+        } else {
+            System.out.println("Invalid input, try again: ");
+            System.out.println("typ 2");
+
+        }
+    userInput.close();
     }
 }
