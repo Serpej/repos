644ae37e97e3355725c435f5ba2f2a39949d938a
Serpej <jehag@live.se>
2025-07-05 23:20:31 +0200

    Add new AlchemySystem project and basic ui inputs

---
+++ AlchemySystem/src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to the Alchemy table!");
+        System.out.println();
+
+        
+    }
+}
---
+++ AlchemySystem/src/ui/ui.java
@@ -0,0 +1,60 @@
+package ui;
+import java.util.Scanner;
+
+public class ui {
+
+    private Scanner userInput;
+
+    public ui() {
+    userInput = new Scanner(System.in);
+    userInput.useDelimiter("[\\s.,]+");
+    }
+
+    public int intInput() {
+        int number = 0;
+        while (true) {
+            if (userInput.hasNextInt()) {
+                number = userInput.nextInt();
+                userInput.nextLine(); 
+                break;
+                } else {
+                System.out.println("Enter numbers only. Try again.");
+                userInput.next();
+            }
+                
+        }
+        return number;
+    }
+
+        public double doubleInput() {
+        double number = 0.0;
+        while (true) {
+
+            if (userInput.hasNextDouble()) {
+                number = userInput.nextDouble();
+                userInput.nextLine();   
+                break;
+                } else {
+                System.out.println("Enter numbers only. Try again.");
+                userInput.next();
+            }
+                
+        }
+        return number;
+    }
+
+    public String stringInput() {
+        String s = "";
+        while (true) {
+            
+            if (userInput.hasNext()) {
+                s = userInput.nextLine();
+                break;
+            } else {
+                System.out.println("Invalid input. Try again.");
+                userInput.next();
+            }
+        }
+        return s;
+    }
+}
