47d57e6eee13f72f109e3fffcf1eacaed0b87131
Serpej <jehag@live.se>
2025-07-06 20:26:31 +0200

    Edit Potion class to aggregate ingredients

--- AlchemySystem/src/potions/Potion.java
+++ AlchemySystem/src/potions/Potion.java
@@ -1,7 +1,45 @@
 package potions;
 
 import ingredients.*;
+import java.util.List;
 
 public class Potion {
+    private String name = "";
+    private String effect = "";
+    private int value = 0;
+    private List<Ingredient> ingredients = null;
 
+    public Potion (String name, String effect, int value, List<Ingredient> ingredients) {
+        this.name = name;
+        this.effect = effect;
+        this.value = value;
+        this.ingredients = ingredients;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEffect() {
+        return effect;
+    }
+    public int getValue() {
+        return value;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    public void describe() {
+        System.out.println("Potion: " + name);
+        System.out.println("Effect: " + effect);
+        System.out.println("Ingredients:");
+        for (Ingredient ingredient : ingredients) {
+            System.out.println();
+            ingredient.getName();
+            ingredient.describe();
+
+        }
+    }
 }
